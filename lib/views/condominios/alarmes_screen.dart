d42d957c4c04e04d512c2c9321545481da03df4c
Eduardo Decussi <eduardo@mkssistemas.com.br>
2025-05-14 17:13:27 -0300

    refactor: adjusted drawer in alarme_screen

--- lib/views/condominios/alarmes_screen.dart
+++ lib/views/condominios/alarmes_screen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mks_app/controller/plantao_controller.dart';
+import 'package:flutter_mks_app/models/plantao_model.dart';
 import 'package:flutter_mks_app/views/widgets/custom_bottom_nav_bar.dart';
+import 'package:intl/intl.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class AlarmesScreen extends StatefulWidget {
   const AlarmesScreen({super.key});
@@ -18,8 +22,186 @@ class _AlarmesScreenState extends State<AlarmesScreen> {
     text: "16/04/2025 10:26",
   );
 
+  final PlantaoController _plantaoController = PlantaoController();
+
   void openDrawer(BuildContext context) {
     scaffoldKey.currentState?.openDrawer();
+  }
+
+  void _showPlantaoDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return StatefulBuilder(
+          builder: (BuildContext context, StateSetter setState) {
+            if (_plantaoController.isLoading) {
+              return const AlertDialog(
+                content: Row(
+                  children: [CircularProgressIndicator(), SizedBox(width: 16)],
+                ),
+              );
+            } else if (_plantaoController.errorMessage != null) {
+              return AlertDialog(
+                title: const Text('Erro'),
+                content: Text(_plantaoController.errorMessage!),
+                actions: [
+                  TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: const Text('Ok'),
+                  ),
+                ],
+              );
+            } else if (_plantaoController.plantaoAtual != null) {
+              final PlantaoAtual plantao = _plantaoController.plantaoAtual!;
+              final DateFormat formatter = DateFormat('dd/MM/yyyy HH:mm');
+              final String dataInicioFormatada = formatter.format(
+                plantao.dataInicio!,
+              );
+              final String dataFinalFormatada = formatter.format(
+                plantao.dataFinal!,
+              );
+
+              return Dialog(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                insetPadding: const EdgeInsets.symmetric(
+                  horizontal: 20,
+                  vertical: 24,
+                ),
+                child: Container(
+                  padding: const EdgeInsets.all(16),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Text(
+                            'Técnicos de Plantão',
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          IconButton(
+                            icon: const Icon(Icons.close),
+                            padding: EdgeInsets.zero,
+                            constraints: const BoxConstraints(),
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                          ),
+                        ],
+                      ),
+                      Text(
+                        '$dataInicioFormatada - $dataFinalFormatada',
+                        style: const TextStyle(fontSize: 14),
+                      ),
+                      const SizedBox(height: 16),
+                      if (plantao.contatoSuporte != null &&
+                          plantao.contatoSuporte!.isNotEmpty)
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Text(
+                              'Contatos - Suporte TI',
+                              style: TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            const SizedBox(height: 8),
+                            ...plantao.contatoSuporte!.map(
+                              (contato) => _buildContactRow(contato),
+                            ),
+                          ],
+                        ),
+                      if (plantao.contatoSuporte != null &&
+                          plantao.contatoSuporte!.isNotEmpty &&
+                          plantao.contatoCampo != null &&
+                          plantao.contatoCampo!.isNotEmpty)
+                        const Divider(),
+                      if (plantao.contatoCampo != null &&
+                          plantao.contatoCampo!.isNotEmpty)
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const SizedBox(height: 8),
+                            const Text(
+                              'Contatos - Campo',
+                              style: TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            const SizedBox(height: 8),
+                            ...plantao.contatoCampo!.map(
+                              (contato) => _buildContactRow(contato),
+                            ),
+                          ],
+                        ),
+                    ],
+                  ),
+                ),
+              );
+            } else {
+              return AlertDialog(
+                content: const Text('Nenhum dado de plantão disponível.'),
+                actions: [
+                  TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: const Text('Ok'),
+                  ),
+                ],
+              );
+            }
+          },
+        );
+      },
+    );
+  }
+
+  Widget _buildContactRow(Contato contato) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 4),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(contato.nome ?? '', style: const TextStyle(fontSize: 14)),
+          Row(
+            children: [
+              Text(
+                contato.telefone ?? '',
+                style: const TextStyle(fontSize: 14),
+              ),
+              const SizedBox(width: 4),
+              InkWell(
+                child: const Icon(Icons.phone, color: Colors.green, size: 20),
+                onTap: () async {
+                  // Remove all non-digit characters from the phone number
+                  final cleanPhone =
+                      contato.telefone?.replaceAll(RegExp(r'[^\d]'), '') ?? '';
+                  print('Clean phone: $cleanPhone');
+
+                  final Uri url = Uri.parse('https://wa.me/55$cleanPhone');
+                  print('URL: $url');
+
+                  if (!await launchUrl(url)) {
+                    throw Exception('Could not launch $url');
+                  }
+                },
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 
   @override
@@ -50,8 +232,8 @@ class _AlarmesScreenState extends State<AlarmesScreen> {
                   begin: Alignment.topLeft,
                   end: Alignment.bottomRight,
                   colors: [
-                    Color.fromARGB(255, 49, 145, 148), // Verde azulado
-                    Color.fromARGB(255, 86, 188, 190), // Verde claro
+                    Color.fromARGB(255, 49, 145, 148),
+                    Color.fromARGB(255, 86, 188, 190),
                   ],
                 ),
               ),
@@ -90,7 +272,7 @@ class _AlarmesScreenState extends State<AlarmesScreen> {
             ),
             ListTile(
               leading: const Icon(Icons.person_2),
-              title: const Text('eDecussi'),
+              title: const Text('Admin'),
               onTap: () {
                 Navigator.pop(context);
               },
@@ -100,13 +282,13 @@ class _AlarmesScreenState extends State<AlarmesScreen> {
               title: const Text('Plantão'),
               onTap: () {
                 Navigator.pop(context);
+                _showPlantaoDialog(context);
               },
             ),
             ListTile(
               leading: const Icon(Icons.logout),
               title: const Text('Sair'),
               onTap: () {
-                Navigator.pop(context);
                 Navigator.pushNamed(context, '/home');
               },
             ),
